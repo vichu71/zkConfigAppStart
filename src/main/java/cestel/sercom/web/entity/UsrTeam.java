5eacfb996c8b9dcc4e5346f7ce035fabf614da70
vichu71 <victor.huecas@gmail.com>
2021-06-24 19:50:53 +0200

    probando subida al repositorio

--- src/main/java/cestel/sercom/web/entity/UsrTeam.java
+++ src/main/java/cestel/sercom/web/entity/UsrTeam.java
@@ -1,8 +1,6 @@
 package cestel.sercom.web.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
